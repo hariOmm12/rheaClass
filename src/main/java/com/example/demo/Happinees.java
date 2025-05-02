b7326ce648abdf9d8d0192267b6a3e260ef50e50
Rupalli Das <rupallidas.job@gmail.com>
2025-05-02 11:01:03 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Happinees.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class Happinees {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		//int beHappy = 2; // Change to false to see the other outcome
+		
+		//String abc = "yes";
+
+		char letter = 'a'; // Assigning a character
+		
+		if (letter=='a') {
+			System.out.println("listen to mom");
+		} else {
+			System.out.println("she will get angry");
+		}
+	}
+
+}
